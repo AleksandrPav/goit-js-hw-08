f46c81b86e7d3ef2dd82c43b525edd81d63c8a7e
Aleksandr Pavlov <pavlovaleksandr@ukr.net>
2022-05-25 22:55:36 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+    const player = Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+    const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+};
