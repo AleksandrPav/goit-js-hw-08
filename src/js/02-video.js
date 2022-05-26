fb15762674dccdcf5a901af89333b2e615da368d
Aleksandr Pavlov <pavlovaleksandr@ukr.net>
2022-05-26 20:58:05 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,28 @@
 import Player from '@vimeo/player';
 
+const iframe = document.querySelector("#vimeo-player")
+const player = new Player(iframe);
 
-const player = new Player('vimeo-player');
-
-
-    player.on('play', function() {
-        console.log('played the video!');
-    });
-
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-
-    const onPlay = function(data) {
-    // data is an object containing properties specific to that event
+const onPlay = (data) => {
+    data = {
+        duration: 61.857,
+        percent: 0.049,
+        seconds: 3.034,
+        customKey: "videoplayer-current-time",
+    }
+    
 };
+let value = player.on('timeupdate', onPlay);
+ 
 
-player.on('play', onPlay);
+
+
+
+
+player.setCurrentTime(value).then(function(seconds) {
+    // seconds = the current playback position
+}).catch(function(error) {
+    // an error occurred
+});
+
+
